11e92000db1839fda3c5a37dda0735aa9f5b186e
masp1234 <mart2c04@stud.kea.dk>
2022-05-05 15:49:08 +0200

    add color attribute to Car

--- src/main/java/com/example/bilabonnement/models/Car.java
+++ src/main/java/com/example/bilabonnement/models/Car.java
@@ -5,12 +5,109 @@ public class Car {
     private String chassisNumber;
     private String make;
     private String model;
+    private String color;
     private String registrationNumber;
     private double registrationFee;
     private String equipmentLevel;
     private double emission;
     private int leaseId;
 
+    public Car() {
+    }
 
+    public Car(int id, String chassisNumber, String make, String model,
+               String color, String registrationNumber,
+               double registrationFee, String equipmentLevel,
+               double emission, int leaseId) {
+        this.id = id;
+        this.chassisNumber = chassisNumber;
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.registrationNumber = registrationNumber;
+        this.registrationFee = registrationFee;
+        this.equipmentLevel = equipmentLevel;
+        this.emission = emission;
+        this.leaseId = leaseId;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getChassisNumber() {
+        return chassisNumber;
+    }
+
+    public void setChassisNumber(String chassisNumber) {
+        this.chassisNumber = chassisNumber;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public double getRegistrationFee() {
+        return registrationFee;
+    }
+
+    public void setRegistrationFee(double registrationFee) {
+        this.registrationFee = registrationFee;
+    }
+
+    public String getEquipmentLevel() {
+        return equipmentLevel;
+    }
+
+    public void setEquipmentLevel(String equipmentLevel) {
+        this.equipmentLevel = equipmentLevel;
+    }
+
+    public double getEmission() {
+        return emission;
+    }
+
+    public void setEmission(double emission) {
+        this.emission = emission;
+    }
+
+    public int getLeaseId() {
+        return leaseId;
+    }
+
+    public void setLeaseId(int leaseId) {
+        this.leaseId = leaseId;
+    }
 }
