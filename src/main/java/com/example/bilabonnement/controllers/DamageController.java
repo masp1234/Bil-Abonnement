2d55dc2c24ba3731a0d749540c7d84a1e18ad072
DNY <dannylu1999@gmail.com>
2022-05-12 19:24:45 +0200

    Added table. No css yet

--- src/main/java/com/example/bilabonnement/controllers/DamageController.java
+++ src/main/java/com/example/bilabonnement/controllers/DamageController.java
@@ -2,15 +2,11 @@ package com.example.bilabonnement.controllers;
 
 
 import com.example.bilabonnement.models.Car;
-import com.example.bilabonnement.models.User;
 import com.example.bilabonnement.services.CarService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import javax.servlet.http.HttpSession;
-import java.rmi.dgc.Lease;
 
 @Controller
 public class DamageController {
@@ -28,7 +24,7 @@ public class DamageController {
         Car car = carService.getCarById(registrationNumber);
         model.addAttribute("car", car);
 
-        return "create-damagerapport";
+        return "create-damagereport";
     }
 
 
