b3b4583769d8ac85e66787ad4c7be8232005d7a5
DNY <dannylu1999@gmail.com>
2022-05-16 22:33:33 +0200

    Tilføjet GetMapping

--- src/main/java/com/example/bilabonnement/controllers/DamageReportController.java
+++ src/main/java/com/example/bilabonnement/controllers/DamageReportController.java
@@ -1,11 +1,24 @@
 package com.example.bilabonnement.controllers;
 
+import com.example.bilabonnement.models.Car;
+import com.example.bilabonnement.services.CarService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 
 public class DamageReportController {
 
+    private CarService carService;
 
+    public DamageReportController(CarService carService) {
+        this.carService = carService;
+    }
 
+    @GetMapping("/show-damagereport/{id}")
+    public String showDamageReport(@PathVariable("id") String registrationNumber) { /*Skal det være registrationNumber siden at det er bilend ID?*/
+        Car car = carService.getCarById(registrationNumber);
+        return "show-damagereport";
+    }
 }
