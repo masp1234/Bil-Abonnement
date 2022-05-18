daf947d7044d663abf0d2347d45d2fc943b542bf
RenasAli <rena0506@stud.kea.dk>
2022-05-18 15:40:35 +0200

    DamageController

--- src/main/java/com/example/bilabonnement/controllers/DamageController.java
+++ src/main/java/com/example/bilabonnement/controllers/DamageController.java
@@ -2,21 +2,30 @@ package com.example.bilabonnement.controllers;
 
 
 import com.example.bilabonnement.models.Car;
+import com.example.bilabonnement.models.Damage;
+import com.example.bilabonnement.models.DamageReport;
 import com.example.bilabonnement.services.CarService;
+import com.example.bilabonnement.services.DamageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class DamageController {
 
     private CarService carService;
+    private DamageService damageService;
 
-    public DamageController (CarService carService) {
+    public DamageController(CarService carService, DamageService damageService) {
         this.carService = carService;
+        this.damageService = damageService;
     }
-
 
     //TODO skal måske modtage kundeId som parameter
     @GetMapping("/create-damagerapport/{id}")
@@ -26,6 +35,38 @@ public class DamageController {
 
 
         return "create-damagereport";
+    }
+    @GetMapping("/damages/{id}")
+    public String getAllDamages(@PathVariable("id") int id, Model model, HttpSession session){
+        Car car= (Car) session.getAttribute("Car");
+        model.addAttribute("Car", car);
+        session.setAttribute("damageReportId", id);
+        List<Damage> damageList= damageService.getAllDamages(id);
+        model.addAttribute("damageList", damageList);
+        DamageReport damageReport= (DamageReport) damageService.getAllDamages(id);
+        model.addAttribute("damageReportObject", damageReport );
+
+
+        return "add-damage";
+    }
+
+    @GetMapping("/add-damage")
+    public String addDamageForm(){
+        return "add-damage";
+    }
+
+    @PostMapping("add-damage")
+    public String addDamage(@RequestParam("description") String description,
+                            @RequestParam("price") int price, HttpSession session){
+        int damageReportId= (int) session.getAttribute("damageReportId");
+        damageService.addDamage(new Damage(description, price, damageReportId));
+        return "redirect:/damages/" + session.getAttribute("damageReportId");
+    }
+
+    @GetMapping("delete-damage/{id}")
+    public String deletedamageById(@PathVariable("id") int id, HttpSession session){
+        damageService.deleteById(id);
+        return "redirect:/damages/" + session.getAttribute("damageReportId");
     }
 
 
