fe82a54f1561b4a6920031e976e57dedba36ca34
DanielKoltze <dani64h6@stud.kea.dk>
2022-05-20 13:23:16 +0200

    Nu kan man oprette en skade

--- src/main/java/com/example/bilabonnement/controllers/DamageController.java
+++ src/main/java/com/example/bilabonnement/controllers/DamageController.java
@@ -21,39 +21,23 @@ import java.util.List;
 @Controller
 public class DamageController {
 
-    private CarService carService;
+
     private DamageService damageService;
     private DamageReportService damageReportService;
 
-    public DamageController(CarService carService, DamageService damageService, DamageReportService damageReportService) {
-        this.carService = carService;
+    public DamageController(DamageService damageService, DamageReportService damageReportService) {
         this.damageService = damageService;
         this.damageReportService = damageReportService;
     }
 
-    //TODO skal måske modtage kundeId som parameter
-    @GetMapping("/create-damagerapport/{id}")
-    public String createLease(@PathVariable("id") String registrationNumber, Model model) {
-        Car car = carService.getCarById(registrationNumber);
-        model.addAttribute("car", car);
-        return "create-damagereport";
-    }
+
     @GetMapping("/damages/{id}")
     public String getAllDamages(@PathVariable("id") int id, Model model, HttpSession session){
         DamageReport damageReport = damageReportService.getDamageReportsById(id);
         model.addAttribute("damageReport", damageReport);
-        Car car= (Car) session.getAttribute("Car");
-        //model.addAttribute("Car", car);
         session.setAttribute("damageReportId", id);
         ArrayList<Damage> damageList= (ArrayList<Damage>) damageService.getAllDamages(id);
         model.addAttribute("damages", damageList);
-
-
-        /*DamageReport damageReport= (DamageReport) damageService.getAllDamages(id);
-        model.addAttribute("damageReportObject", damageReport );*/
-
-
-
         return "show-damages";
     }
 
@@ -64,10 +48,10 @@ public class DamageController {
 
     @PostMapping("add-damage")
     public String addDamage(@RequestParam("description") String description,
-                            @RequestParam("price") int price, HttpSession session){
+                            @RequestParam("price") int price,HttpSession session){
         int damageReportId= (int) session.getAttribute("damageReportId");
         damageService.addDamage(new Damage(description, price, damageReportId));
-        return "redirect:/damages/" + session.getAttribute("damageReportId");
+        return "redirect:/damages/" + damageReportId;
     }
 
     @GetMapping("delete-damage/{id}")
