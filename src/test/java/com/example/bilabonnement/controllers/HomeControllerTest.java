6ad4bfd10a1aa3fc23f16502674f1e31f9c331b8
masp1234 <mart2c04@stud.kea.dk>
2022-05-20 10:18:25 +0200

    add webapp test

--- src/test/java/com/example/bilabonnement/controllers/HomeControllerTest.java
+++ src/test/java/com/example/bilabonnement/controllers/HomeControllerTest.java
@@ -1,34 +1,67 @@
 package com.example.bilabonnement.controllers;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.example.bilabonnement.services.UserService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-
-@WebMvcTest(HomeController.class)
 class HomeControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    public void index() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("brugernavn")))
-                .andExpect(content().string(containsString("kodeord")))
-                .andExpect(content().string(containsString("Indsæt")));
-
+    @BeforeAll
+    //Skal være static
+    public static void setup() {
+        //sætter automatisk WebDriver op, så man ikke skal downloade filen og bekymre sig om, om det er mac eller win
+        WebDriverManager.chromedriver().setup();
     }
 
+    @Test
+    public void firstTest() {
 
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://bil-abonnement-projekt.herokuapp.com");
+
+        //  WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement username = driver.findElement(By.name("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        // WebElement loginButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("login-button")));
+
+        username.sendKeys("Bobsen");
+        password.sendKeys("123123");
+        loginButton.click();
+
+
+        WebElement createCustomerButton = driver.findElement(By.className("createCustomer"));
+        createCustomerButton.click();
+
+        WebElement cpr = driver.findElement(By.id("cpr"));
+        cpr.sendKeys("2121212");
+
+        WebElement firstName = driver.findElement(By.id("firstName"));
+        firstName.sendKeys("Henning");
+
+        WebElement lastName = driver.findElement(By.id("lastName"));
+        lastName.sendKeys("Henningsen");
+
+        WebElement email = driver.findElement(By.id("email"));
+        email.sendKeys("Henning@gmail.com");
+
+        WebElement phoneNumber = driver.findElement(By.id("phoneNumber"));
+        phoneNumber.sendKeys("9349354");
+
+        WebElement address = driver.findElement(By.id("address"));
+        address.sendKeys("dfsfsf vej 21");
+
+        WebElement zipCode = driver.findElement(By.id("zipCode"));
+        zipCode.sendKeys("2630");
+
+        WebElement createCustomerSubmitButton = driver.findElement(By.className("button-submit"));
+
+        createCustomerSubmitButton.click();
+
+    }
 
 }
